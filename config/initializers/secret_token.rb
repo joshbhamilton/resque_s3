5c0d6da148c74b590de688cf66a546f9093994b3
Josh Hamilton <joshb.hamilton@gmail.com>
2011-03-16 16:47:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueS3::Application.config.secret_token = '140f7a648dd85a68835c503b9a39eee50ac1df93bf16048dd06eebeeef9072ed7575b91b974bfba4c2ebf7126992e8a00c9ff820849878c49aeb3a11a0e2bb36'
